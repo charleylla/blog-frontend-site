b917dfe0ab1c903532e7321a3b8341eaf91f20e3
Charley <charleylla@foxmail.com>
2018-02-08 11:12:37 +0800

    add components dir in TimeAxis.

---
+++ components/TimeAxis/components/TimeLineArrow/index.js
@@ -0,0 +1,33 @@
+import React,{ Component } from "react";
+import stylesheet from "./style.css";
+
+export default class TimeLineArrow extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+
+        }
+        this._initbind();
+        this._init();
+    }
+
+    _initbind(){
+       
+    }
+
+    _init(){
+
+    }
+    
+    componentDidMount(){
+
+    }
+
+    render(){
+        return(
+            <div className="arrow__main">
+
+            </div>
+        );
+    }
+}
